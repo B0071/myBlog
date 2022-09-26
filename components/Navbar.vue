ed68137b7262ed2646deaf6d5bd573fcb967ee48
Bekhruz_Sharapov <BekhruzSHofficial@gmail.com>
2022-09-26 17:41:16 +0500

    first commit

---
+++ components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Hello
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'NavBar',
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="main">
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+    export default {
+    name: "DefaultLayout",
+}
+</script>
+
+<style>
+    .main{
+        width: 98%;
+        margin: auto;
+    }
+</style>
